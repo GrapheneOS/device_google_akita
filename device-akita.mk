ba9d94cafdde83d291090caac5e50dcfba0d7746
Ken Yang <yangken@google.com>
2022-12-22 07:28:33 +0000

    WLC: Include the wireless_charger from gs-common
    Bug: 237600973
    Change-Id: I0a6571f7a36cadba8db57d02d8e66e17bbe80cb6
    Signed-off-by: Ken Yang <yangken@google.com>

--- device-akita.mk
+++ device-akita.mk
@@ -27,6 +27,7 @@ include device/google/akita/audio/akita/audio-tables.mk
 include device/google/akita/vibrator/cs40l26/device-akita.mk
 include device/google/gs-common/bcmbt/bluetooth.mk
 include device/google/gs-common/touch/gti/gti.mk
+include device/google/gs-common/wireless_charger/wireless_charger.mk
 
 # go/lyric-soong-variables
 $(call soong_config_set,lyric,camera_hardware,akita)
