3269c3e47fdb4bb9b666b82dbcf0511e4dbd8167
David Chao <davidchao@google.com>
2023-03-16 14:41:40 +0800

    thermal: seperate thermal config for charging
    - Separate thermal config for charger mode and remove the unused thermal
    throttling.
    
    Bug: 272701980
    Test: charging throttling worked properly in charger.
    Change-Id: I6a13fccd5981cef561d02fddbb19847801e34883

--- device-akita.mk
+++ device-akita.mk
@@ -76,7 +76,8 @@ DEVICE_MANIFEST_FILE += \
 
 # Thermal Config
 PRODUCT_COPY_FILES += \
-	device/google/akita/thermal_info_config_akita.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+	device/google/akita/thermal_info_config_akita.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
+	device/google/akita/thermal_info_config_charge_akita.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_charge.json
 
 # Power HAL config
 PRODUCT_COPY_FILES += \
