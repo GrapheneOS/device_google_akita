338624e66a989349450702aebc77a172ce889644
chenkris <chenkris@google.com>
2023-06-13 03:09:54 +0000

    udfps: fix BB on git_master-without-vendor
    Bug: 286774587
    Test: Build
    Change-Id: Ied3239fa1dfa4bc30f8f10fc3e3f987c580ee9ab

--- device-akita.mk
+++ device-akita.mk
@@ -188,11 +188,11 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Fingerprint HAL
 GOODIX_CONFIG_BUILD_VERSION := g7_trusty
-include vendor/goodix/udfps/configuration/udfps_common.mk
+$(call inherit-product-if-exists, vendor/goodix/udfps/configuration/udfps_common.mk)
 ifeq ($(filter factory%, $(TARGET_PRODUCT)),)
-include vendor/goodix/udfps/configuration/udfps_shipping.mk
+$(call inherit-product-if-exists, vendor/goodix/udfps/configuration/udfps_shipping.mk)
 else
-include vendor/goodix/udfps/configuration/udfps_factory.mk
+$(call inherit-product-if-exists, vendor/goodix/udfps/configuration/udfps_factory.mk)
 endif
 
 PRODUCT_VENDOR_PROPERTIES += \
