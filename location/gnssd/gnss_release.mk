bc0094b214a693f082930a2e411e865b6a9223ab
Edwin Tung <edwintung@google.com>
2023-11-24 12:19:01 +0800

    gps: update gps version 4.13.1.37
    Swap MIDs for GPIO_ON and USI_ENABLE
    Selective separate binaries
    Disable HSI on K41 when not in use
    
    Bug: 224481022
    Bug: 281352318
    
    Test: b/313001472
    atest CtsLocationGnssTestCases
    atest VtsHalGnssTargetTest
    
    Change-Id: I5bfa51db93203b90b0e90b040e85d1c4b1284f48

--- location/gnssd/gnss_release.mk
+++ location/gnssd/gnss_release.mk
@@ -7,7 +7,8 @@ PRODUCT_PACKAGES += \
     android.hardware.location.gps.prebuilt.xml
 
 PRODUCT_COPY_FILES += \
-    device/google/akita/location/gnssd/release/ca.pem:vendor/etc/gnss/ca.pem
+    device/google/akita/location/gnssd/release/ca.pem:vendor/etc/gnss/ca.pem \
+    device/google/akita/location/gnssd/release/kepler.bin:vendor/firmware/kepler.bin
 
 PRODUCT_SOONG_NAMESPACES += \
     device/google/akita/location/gnssd
