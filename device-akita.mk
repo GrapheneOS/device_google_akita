0c388419240f8be23a1930400db699c2ce8bd783
Cyan_Hsieh <cyanhsieh@google.com>
2024-04-08 10:55:19 +0800

    Update Akita SVN to 7
    Bug: 325984251
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b48319f3783931c60384351c629cabd68cdb6b92)
    Merged-In: Ifa6fcf862ec5cbd01a8e46571634f0dddee406ea
    Change-Id: Ifa6fcf862ec5cbd01a8e46571634f0dddee406ea

--- device-akita.mk
+++ device-akita.mk
@@ -357,7 +357,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=6
+    ro.vendor.build.svn=7
 
 # Keyboard height ratio and bottom padding in dp for portrait mode
 PRODUCT_PRODUCT_PROPERTIES += \
