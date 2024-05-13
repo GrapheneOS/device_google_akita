878142e02e5a0ea4af1dc3e1148d010a1ef68181
Ravi Jain <raviajain@google.com>
2024-05-13 08:36:46 +0000

    Vibrator: Set Soong Config variable - kernel_ver
    Bug: 340148508
    Test: Build and check fw files in all devices.
    Change-Id: Iced0ba4a08a67f4331bc9250e7ffbbac5b287fdb

--- device-akita.mk
+++ device-akita.mk
@@ -369,6 +369,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.camera.ois_with_system_imu=true
 
 # Vibrator HAL
+$(call soong_config_set,haptics,kernel_ver,v$(subst .,_,$(TARGET_LINUX_KERNEL_VERSION)))
 ADAPTIVE_HAPTICS_FEATURE := adaptive_haptics_v1
 PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.vibrator.hal.supported_primitives=243 \
