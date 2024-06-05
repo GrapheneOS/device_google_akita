be8299afa7b31c1ea6e9527f12626717f9f03f32
Ankur Bakshi <ankurbakshi@google.com>
2024-06-05 20:23:31 +0000

    Update Akita SVN to 15
    Bug: 336526332
    Change-Id: I86942ff5e49632bc7813b895d19c7bc8a554ff81

--- device-akita.mk
+++ device-akita.mk
@@ -383,7 +383,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=14
+    ro.vendor.build.svn=15
 
 # Keyboard height ratio and bottom padding in dp for portrait mode
 PRODUCT_PRODUCT_PROPERTIES += \
