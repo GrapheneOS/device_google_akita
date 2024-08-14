76f57fdd804f04395f6fac2e3c81897486cf8c48
Ankur Bakshi <ankurbakshi@google.com>
2024-08-14 23:31:14 +0000

    Update Akita SVN to 19
    Bug: 352170103
    Flag: EXEMPT bugfix
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:392c47e3aa7e90007e282feaa6af0e6417d85637)
    Merged-In: I54a6a09bc86975cd77b2f809c931ea90f991c6e0
    Change-Id: I54a6a09bc86975cd77b2f809c931ea90f991c6e0

--- device-akita.mk
+++ device-akita.mk
@@ -389,7 +389,7 @@ ifdef RELEASE_SVN_AKITA
 TARGET_SVN ?= $(RELEASE_SVN_AKITA)
 else
 # Set this for older releases that don't use build flag
-TARGET_SVN ?= 18
+TARGET_SVN ?= 19
 endif
 
 PRODUCT_VENDOR_PROPERTIES += \
