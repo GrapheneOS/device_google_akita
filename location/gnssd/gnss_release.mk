48fc8c0bc11b47da3e5b78b9250f8c527492900f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-21 22:46:39 +0300

    don't copy vendor/etc/gnss/ca.pem from AOSP
    It's extracted from stock OS image instead.

--- location/gnssd/gnss_release.mk
+++ location/gnssd/gnss_release.mk
@@ -7,7 +7,6 @@ PRODUCT_PACKAGES += \
     android.hardware.location.gps.prebuilt.xml
 
 PRODUCT_COPY_FILES += \
-    device/google/akita/location/gnssd/release/ca.pem:vendor/etc/gnss/ca.pem \
     device/google/akita/location/gnssd/release/kepler.bin:vendor/firmware/kepler.bin
 
 PRODUCT_SOONG_NAMESPACES += \
